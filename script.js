455329ac23531ff4c9416945e899051a1222b93e
kubier <opisnerf@gmail.com>
2025-01-20 16:40:28 +0200

    DOM display logic

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const gameboard = (function() {
 
     function setBoard(marker, x, y) {
         board[y][x] = marker
+
+        return board
     }
 
     function checkWin() {
@@ -35,10 +37,43 @@ const gameboard = (function() {
     return {setBoard, checkWin}
 })();
 
+const displayController = (function() {
+    const boardContainer = document.querySelector('.board')
+
+    function update(board) {
+        boardContainer.textContent = ''
+
+        for (let y = 0; y < 3; y++) {
+            const newLayer = document.createElement('div')
+            newLayer.classList.add('layer')
+            newLayer.setAttribute('data', y)
+            boardContainer.appendChild(newLayer)
+
+            for (let x = 0; x < 3; x++) {
+                const newCell = document.createElement('div')
+                newCell.classList.add('cell')
+                newCell.setAttribute('data', x)
+                if (board[y][x]) {
+                    newCell.textContent = board[y][x]
+                } else {
+                    newCell.textContent = ' '
+                }
+                newCell.textContent = board[y][x]
+                newLayer.appendChild(newCell)
+            }
+            
+        }
+    }
+
+    return {update}
+})()
+
 function makePlayer(marker) {
     function play(x, y) {
-        gameboard.setBoard(marker, x, y)
+        return gameboard.setBoard(marker, x, y)
     }
 
     return {play}
 }
+
+displayController.update(makePlayer('O').play(1,0))
